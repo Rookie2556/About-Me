3f31c1e0f916e4ab303eba9bf81bbb2cccfb8548
Thomas M <122787483+Rookie2556@users.noreply.github.com>
2023-01-24 14:19:11 +0000

    Usign strict and put all scripts here

--- Prompter.js
+++ Prompter.js
@@ -1,1 +1,84 @@
-//
+//trying to put all js into this file, and add use strict.
+'use strict';
+
+<script>
+        let name = prompt("What is my name? ").toLowerCase();
+        if (name == "thomas") {
+            console.log("They think my name is " + name + "They were right!");
+            alert("You are correct!")
+            document.write("You are correct!");
+        } else {
+            console.log("They think my name is " + name + "They were Wrong!");
+            alert("You are wrong! ");
+            document.write("You are wrong! ");
+        }
+    </script>
+
+    <br>
+  
+   <script>
+        let colour = prompt("What is my favourite color? ").toLowerCase();
+        if (colour == "green") {
+            console.log("They think my favourite colour is " + colour + "They were right!");
+            alert("You are correct!")
+            document.write("You are correct!");
+        } else {
+            console.log("They think my favourite color is " + colour + "They were Wrong!");
+            alert("You are wrong! ");
+            document.write("You are wrong! ");
+        }
+      </script>
+  
+  <br> 
+  
+  <script>
+        let location = prompt("Where do I live? ").toLowerCase();
+        if (location == "liverpool") {
+            console.log("They think live in " + location + "They were right!");
+            alert("You are correct!")
+            document.write("You are correct!");
+        } else {
+            console.log("They think live in  " + location + "They were Wrong!");
+            alert("You are wrong! ");
+            document.write("You are wrong! ");
+        }
+      </script>
+  
+  <br> 
+  
+   <script>
+        let disability = prompt("I have a disability what is it? ").toLowerCase();
+        if (disability == "deaf") {
+            console.log("They think my disability is " + disabiltiy + "They were right!");
+            alert("You are correct!")
+            document.write("You are correct!");
+        } else {
+            console.log("They think my disability is  " + disability + "They were Wrong!");
+            alert("You are wrong! ");
+            document.write("You are wrong! ");
+        }
+      </script>
+  
+  <br> 
+  
+  <script>
+        let faveGame = prompt("What is my favourite game? ").toLowerCase();
+        if (faveGame == "rl") {
+            console.log("They think my favourite game is " + faveGame + "They were right!");
+            alert("You are correct!")
+            document.write("You are correct!");
+        } else {
+            console.log("They think my favourite game is  " + faveGame + "They were Wrong!");
+            alert("You are wrong! ");
+            document.write("You are wrong! ");
+        }
+      </script>
+
+  <script>
+        let theyName = prompt("What is Your name? ");
+    {
+            console.log("Their name is " + theyName);
+            alert("You are " + theyName + " Welcome to my website, I am Thomas and read on to find out more about me! ");
+  
+  </script>
+  
