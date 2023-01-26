e15a716b59218186b49391eddda2966d3d1d0349
Thomas M <122787483+Rookie2556@users.noreply.github.com>
2023-01-26 15:07:18 +0000

    changed location to avoiderrror

--- Prompter.js
+++ Prompter.js
@@ -1,47 +1,46 @@
-
-        let name = prompt("What is my name? ").toLowerCase();
-        if (name == "thomas") {
-            console.log("They think my name is " + name + "They were right!");
-            alert("You are correct!")
-            document.write("You are correct!");
-        } else {
-            console.log("They think my name is " + name + "They were Wrong!");
-            alert("You are wrong! ");
-            document.write("You are wrong! ");
-        }
-  
-        let colour = prompt("What is my favourite color? ").toLowerCase();
-        if (colour == "green") {
-            console.log("They think my favourite colour is " + colour + "They were right!");
-            alert("You are correct!")
-            document.write("You are correct!");
-        } else {
-            console.log("They think my favourite color is " + colour + "They were Wrong!");
-            alert("You are wrong! ");
-            document.write("You are wrong! ");
-        }
-  
-        let location = prompt("Where do I live? ").toLowerCase();
-        if (location == "liverpool") {
-            console.log("They think live in " + location + "They were right!");
-            alert("You are correct!")
-            document.write("You are correct!");
-        } else {
-            console.log("They think live in  " + location + "They were Wrong!");
-            alert("You are wrong! ");
-            document.write("You are wrong! ");
-        }
- 
-       let disability = prompt("I have a disability what is it? ").toLowerCase();
-    if (disability == "deaf") {
-        console.log("They think my disability is " + disability + "They were right!");
+    let name = prompt("What is my name? ").toLowerCase();
+    if (name == "thomas") {
+        console.log("They think my name is " + name + "They were right!");
         alert("You are correct!")
         document.write("You are correct!");
     } else {
-        console.log("They think my disability is  " + disability + "They were Wrong!");
+        console.log("They think my name is " + name + "They were Wrong!");
         alert("You are wrong! ");
         document.write("You are wrong! ");
     }
+
+    let colour = prompt("What is my favourite color? ").toLowerCase();
+    if (colour == "green") {
+        console.log("They think my favourite colour is " + colour + "They were right!");
+        alert("You are correct!")
+        document.write("You are correct!");
+    } else {
+        console.log("They think my favourite color is " + colour + "They were Wrong!");
+        alert("You are wrong! ");
+        document.write("You are wrong! ");
+    }
+
+    let locationOfResidence = prompt("Where do I live? ").toLowerCase();
+    if (locationOfResidence == "liverpool") {
+        console.log("They think live in " + locationOfResidence + "They were right!");
+        alert("You are correct!")
+        document.write("You are correct!");
+    } else {
+        console.log("They think live in  " + locationOfResidence + "They were Wrong!");
+        alert("You are wrong! ");
+        document.write("You are wrong! ");
+    }
+
+   let disability = prompt("I have a disability what is it? ").toLowerCase();
+if (disability == "deaf") {
+    console.log("They think my disability is " + disability + "They were right!");
+    alert("You are correct!")
+    document.write("You are correct!");
+} else {
+    console.log("They think my disability is  " + disability + "They were Wrong!");
+    alert("You are wrong! ");
+    document.write("You are wrong! ");
+}
 
 
     let faveGame = prompt("What is my favourite game? ").toLowerCase();
